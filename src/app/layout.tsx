019fee6e91b2af2bb4a048714d97105a3ac3c81f
Assoghe <soraya.gherras17@gmail.com>
2024-04-14 19:17:38 +0200

    Config > Replace Inter font for Unbounded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Unbounded } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const unbounded = Unbounded({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Soraya Gherras",
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body className={unbounded.className}>{children}</body>
     </html>
   );
 }
