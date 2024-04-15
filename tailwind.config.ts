98f0c2a76d18c8a4aa813f08409ffbf113e99e56
Assoghe <soraya.gherras17@gmail.com>
2024-04-15 10:18:52 +0200

    Typography > Add font size in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,56 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-  // Or if using `src` directory:
+  content: [
   "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      'sm': ['0.9375rem', {
+        lineHeight: '1.8rem',
+        letterSpacing: '0.02813rem',
+        fontWeight: '300',
+      }],
+      'base': ['1.125rem', {
+        lineHeight: '1.8rem',
+        letterSpacing: '0.03375rem',
+        fontWeight: '300',
+      }],
+      'lg': ['0.93rem', {
+        lineHeight: '1.575rem',
+        letterSpacing: '0.0375rem',
+        fontWeight: '400',
+      }],
+      'xl': ['0.9375rem', {
+        lineHeight: '1.40625rem',
+        letterSpacing: '0.075rem',
+        fontWeight: '400',
+      }],
+      '2xl': ['1.125rem', {
+        lineHeight: '2.7rem',
+        letterSpacing: '0.045rem',
+        fontWeight: '500',
+      }],
+      '3xl': ['1.3125rem', {
+        lineHeight: '2.7rem',
+        letterSpacing: '0.105rem',
+        fontWeight: '400',
+      }],
+      '4xl': ['2.25rem', {
+        lineHeight: '3.6rem',
+        letterSpacing: '0.18rem',
+        fontWeight: '400',
+      }],
+      '5xl': ['2.5rem', {
+        lineHeight: '4.5rem',
+        letterSpacing: '0.1rem',
+        fontWeight: '400',
+      }],
+    },
+
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      
     },
   },
   plugins: [],
