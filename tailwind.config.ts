5f41211b00e64c59c06d156806206995e2acedf0
Assoghe <soraya.gherras17@gmail.com>
2024-05-05 23:25:07 +0200

    Tailwind > Fix lineHeight font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,42 +12,42 @@ const config: Config = {
     },
     fontSize: {
       'sm': ['0.9375rem', {
-        lineHeight: '1.8rem',
+        lineHeight: '1.4rem',
         letterSpacing: '0.02813rem',
         fontWeight: '300',
       }],
-      'base': ['1rem', {
-        lineHeight: '1.8rem',
+      'base': ['0.9rem', {
+        lineHeight: '1.5rem',
         letterSpacing: '0.03375rem',
         fontWeight: '300',
       }],
       'lg': ['0.93rem', {
-        lineHeight: '1.575rem',
+        lineHeight: '1.3rem',
         letterSpacing: '0.0375rem',
         fontWeight: '400',
       }],
       'xl': ['0.9375rem', {
-        lineHeight: '1.40625rem',
+        lineHeight: '1.3em',
         letterSpacing: '0.075rem',
         fontWeight: '400',
       }],
       '2xl': ['1.125rem', {
-        lineHeight: '2.7rem',
+        lineHeight: '2.3rem',
         letterSpacing: '0.045rem',
         fontWeight: '400',
       }],
       '3xl': ['1.3125rem', {
-        lineHeight: '2.7rem',
+        lineHeight: '2.4rem',
         letterSpacing: '0.105rem',
         fontWeight: '400',
       }],
       '4xl': ['1.8rem', {
-        lineHeight: '3.6rem',
+        lineHeight: '3rem',
         letterSpacing: '0.18rem',
         fontWeight: '400',
       }],
       '5xl': ['2.3rem', {
-        lineHeight: '4.5rem',
+        lineHeight: '3.5rem',
         letterSpacing: '0.1rem',
         fontWeight: '400',
       }],
