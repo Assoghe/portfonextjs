52fe0c8a8dab6011c942cfb9132b2c620e8f412f
Assoghe <soraya.gherras17@gmail.com>
2024-05-06 02:05:05 +0200

    Add key

--- src/app/daily/page.tsx
+++ src/app/daily/page.tsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import { dailyData } from '../constants/constants'
-import Image from 'next/image'
-import { Typography } from '../ui/design-system/typography/typography'
-import InfoSection from '../[case-study]/_component/InfoSection'
-import HeadingSection from '../[case-study]/_component/Heading'
+import React from 'react';
+import { dailyData } from '../constants/constants';
+import Image from 'next/image';
+import { Typography } from '../ui/design-system/typography/typography';
+import InfoSection from '../[case-study]/_component/InfoSection';
+import HeadingSection from '../[case-study]/_component/Heading';
 
 export default function Daily() {
     return (
         <div>
-            {
-                dailyData.map((dailyItem, index) => (
-                    <>
-                   {dailyData.map((daily) => (
-                        <HeadingSection
-                            title={daily.name}
-                            text={daily.description}
-                            src={daily.image}
-                        />
-                   ))
-                   }
-                        <div className='grid tablet:grid-cols-3 grid-cols-1 gap-9 px-6 tablet:px-40' key={index}>
-                            {dailyItem.dailyCases.map((dailyCase, index) => (
-                                <div key={index}>
-                                    <Image className='rounded-md'
-                                        src={dailyCase.dailyImg}
-                                        alt={`Daily Challenge ${index + 1}`}
-                                        width={0}
-                                        height={0}
-                                        sizes="100vw"
-                                        style={{ width: '100%', height: 'auto' }}
-                                    />
-                                    <Typography variant='h4' component='h4'>{dailyCase.dailyNumber}</Typography>
-                                    <Typography variant="body-one" component='p' className='text-neutral-300'>{dailyCase.dailyTitle}</Typography>
-                                </div>
-                            ))}
-                        </div>
-                    </>
-
-                        ))
-            }
-                    </div >
-    )
+            {dailyData.map((dailyItem, index) => (
+                <div key={index}>
+                    <HeadingSection
+                        title={dailyItem.name}
+                        text={dailyItem.description}
+                        src={dailyItem.image}
+                    />
+                    <div className='grid tablet:grid-cols-3 grid-cols-1 gap-9 px-6 tablet:px-40'>
+                        {dailyItem.dailyCases.map((dailyCase, subIndex) => (
+                            <div key={subIndex}>
+                                <Image
+                                    className='rounded-md'
+                                    src={dailyCase.dailyImg}
+                                    alt={`Daily Challenge ${subIndex + 1}`}
+                                    width={0}
+                                    height={0}
+                                    sizes="100vw"
+                                    style={{ width: '100%', height: 'auto' }}
+                                />
+                                <Typography variant='h4' component='h4'>
+                                    {dailyCase.dailyNumber}
+                                </Typography>
+                                <Typography variant="body-one" component='p' className='text-neutral-300'>
+                                    {dailyCase.dailyTitle}
+                                </Typography>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
