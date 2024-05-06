2d9b08c96da0dff544307a7eabbf9982ade821dc
Assoghe <soraya.gherras17@gmail.com>
2024-05-07 00:19:24 +0200

    Heading.tsx > Add

--- src/app/[case-study]/_component/Heading.tsx
+++ src/app/[case-study]/_component/Heading.tsx
@@ -6,27 +6,30 @@ import Divider from '@/app/ui/design-system/divider/divider';
 interface HeadingSectionProps {
     title?: string;
     text?: string;
-    src: string;
+    src?: string;
     alt?: string;
 }
 
 const HeadingSection: React.FC<HeadingSectionProps> = ({ title, text, src, alt }) => (
     <div className='px-6 tablet:px-40 mt-72'>
-       <Typography variant='h1' component='h1'>
+        <Typography variant='h1' component='h1'>
             {title}
-       </Typography>
+        </Typography>
         <Typography variant='body-one' component='p' className='mb-12 text-neutral-300'>
             {text}
         </Typography>
 
-        <Image className='mt-9 mb-20 rounded-xl drop-shadow-md'
-            src={src}
-            alt={alt || ''}
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: 'auto' }}
-        />
+        {src && (
+            <Image
+                className='mt-9 mb-20 rounded-xl drop-shadow-md'
+                src={src}
+                alt={alt || ''}
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: 'auto' }}
+            />
+        )}
 
     </div>
 );
