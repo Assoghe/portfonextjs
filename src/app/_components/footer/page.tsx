75ee709dde13e3c0ffc6b29a3d34c6ba632b5351
Assoghe <soraya.gherras17@gmail.com>
2024-05-07 15:52:02 +0200

    Fix footer

--- src/app/_components/footer/page.tsx
+++ src/app/_components/footer/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='bg-primary px-6 tablet:px-52  text-center py-2'>
-        <Typography component='p' className='text-neutral-950 text-xs'>
+        <Typography component='p' variant='body-three' className='text-neutral-950'>
         © Soraya Gherras | Tous droits réservés | Développé avec Next.js, TypeScript et Tailwind.
         </Typography>
     </footer>
